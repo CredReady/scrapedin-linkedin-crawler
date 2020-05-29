2ded7083ca380d2d8e67b2ece8a780494c8c1247
Thomas Proctor <proctortc@gmail.com>
2020-05-29 16:42:22 -0400

    delay and limit

--- src/crawler.js
+++ src/crawler.js
@@ -4,14 +4,14 @@ const dependencies = {
   scrapProfile: require('./scrapProfile')
 }
 
-const WORKER_INTERVAL_MS = 1000
-
 module.exports = async (profileScraper, rootProfiles, injection) => new Promise((resolve) => {
   const {
     config,
     scrapProfile
   } = Object.assign({}, dependencies, injection)
 
+  const WORKER_INTERVAL_MS = config.interProfileWaitTime
+  const MAX_PROFILES_SCRAPED = config.maxProfilesScraped
   let currentProfilesToCrawl = rootProfiles
   let alreadyCrawledProfiles = new Set(rootProfiles)
   let nextProfilesToCrawl = new Set()
@@ -33,17 +33,23 @@ module.exports = async (profileScraper, rootProfiles, injection) => new Promise(
         parallelCrawlers--
       })
       .catch((e) => {
-        logger.error(`error on crawling profile: ${profileUrl} \n ${e}`)
+	  logger.error(`error on crawling profile: ${profileUrl} \n ${e}`)
+	  logger.error(e.stack)
         parallelCrawlers--
       })
   }
 
   setInterval(() => {
     if (currentProfilesToCrawl.length === 0 && nextProfilesToCrawl.size === 0) {
-      logger.info('there is no profiles to crawl right now...')
+	logger.info('there is no profiles to crawl right now...')
     } else if (currentProfilesToCrawl.length === 0) {
       logger.info(`a depth of crawling was finished, starting a new depth with ${nextProfilesToCrawl.size} profile(s)`)
 	currentProfilesToCrawl = Array.from(nextProfilesToCrawl)
+	if (alreadyCrawledProfiles.size + currentProfilesToCrawl.length > MAX_PROFILES_SCRAPED) {
+	    logger.info('Hit Max Profiles.')
+	    let nNewProfiles = MAX_PROFILES_SCRAPED - alreadyCrawledProfiles.size
+	    currentProfilesToCrawl = currentProfilesToCrawl.slice(0, nNewProfiles)
+	}
 	alreadyCrawledProfiles = union(alreadyCrawledProfiles, new Set(currentProfilesToCrawl))
       nextProfilesToCrawl = new Set()
     } else if (parallelCrawlers < config.maxConcurrentCrawlers) {
